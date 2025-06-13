db47bab43124c8d3e1c47a2f30cbbc415b78fe49
babayigit <babayigit@gmail.com>
2025-06-13 22:33:28 +0300

    3.v

--- src/test/tests/P01_test.java
+++ src/test/tests/P01_test.java
@@ -1,4 +1,7 @@
 package test.tests;
 
 public class P01_test {
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/tests/P02_tests.java
+++ src/test/tests/P02_tests.java
@@ -1,4 +1,7 @@
 package test.tests;
 
 public class P02_tests {
+    public static void main(String[] args) {
+
+    }
 }
